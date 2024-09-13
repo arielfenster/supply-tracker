053743c67e89b0e764c4b5138ba4c31d8a665df6
Ariel Fenster <arielfenster1610@gmail.com>
2024-09-13 20:43:13 +0300

    some issues with pg connection

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,7 +3,7 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schemas from './schemas';
 
-const client = postgres(env.server.DATABASE_URL);
+const client = postgres(env.server.DATABASE_URL, { max: 1 });
 const db = drizzle(client, { schema: schemas });
 
 export type Database = typeof db;
